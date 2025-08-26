b54a232cf56a50b7272572eea9d29359dbd2bcd0
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2025-08-26 17:58:37 +0100

    Implement user auth

---
+++ src/services/auth.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL
+})
+
+api.interceptors.request.use((config) => {
+  const authToken = localStorage.getItem('token');
+  if(authToken) {
+    config.headers.Authorization = `${authToken}`
+  }
+  return config
+})
+
+api.interceptors.response.use((response) => response, (error) => {
+  if(error.response?.status === 401) {
+    localStorage.removeItem('token')
+    window.location.href = "/login"
+  }
+  return Promise.reject(error)
+})
+
+export const auth = {
+  login: async (email, password) => {
+    const response = await api.post('/user/sign_in', { email, password })
+    const { token } = response.data
+    localStorage.setItem('token', token)
+    return response.data;
+  },
+
+  logout: () => {
+    localStorage.removeItem('token')
+  },
+
+  isAuthenticated: () => {
+    return !!localStorage.getItem('token')
+  }
+}
+
+export default api
